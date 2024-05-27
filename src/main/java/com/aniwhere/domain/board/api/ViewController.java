e5e0dfe803ce239e2140c2065cd2b50fbfb9b3f1
patrick <lusielar@naver.com>
2024-05-28 00:15:51 +0900

    프로젝트 구조 설계 (#1) - patrick

--- src/main/java/com/aniwhere/domain/board/api/ViewController.java
+++ src/main/java/com/aniwhere/domain/board/api/ViewController.java
@@ -1,8 +1,10 @@
 package com.aniwhere.domain.board.api;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller("boardViewController")
+@RequestMapping("/board")
 public class ViewController {
 
 
