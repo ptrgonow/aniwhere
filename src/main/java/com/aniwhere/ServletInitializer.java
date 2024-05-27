35a3aa4a9f2db2fabbfeba1aea24f3ec10f07ce0
patrick <lusielar@naver.com>
2024-05-27 14:00:29 +0900

    프로젝트 구조 설계 (#1) - patrick

---
+++ src/main/java/com/aniwhere/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.aniwhere;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AniwhereApplication.class);
+    }
+
+}
