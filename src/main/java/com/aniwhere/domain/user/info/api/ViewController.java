e618a1132c1e0794c4d7533bb14d8ce2b68a5eb8
hnsangjin <uk96dhk1@gmail.com>
2024-05-28 16:38:56 +0900

    프로젝트 구조 설계 (#1) - hsj

--- src/main/java/com/aniwhere/domain/user/info/api/ViewController.java
+++ src/main/java/com/aniwhere/domain/user/info/api/ViewController.java
@@ -1,9 +1,12 @@
 package com.aniwhere.domain.user.info.api;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller("userInfoViewController")
 @RequestMapping("/user")
 public class ViewController {
+
+
 }
