933a5703a951b2d6b2b94837e7d939f8a1f3c8aa
coconut9704 <yjs9704@naver.com>
2024-05-28 17:03:08 +0900

    프로젝트 구조 설계 (#1-1) - jinseok

--- src/main/java/com/aniwhere/domain/board/api/ViewController.java
+++ src/main/java/com/aniwhere/domain/board/api/ViewController.java
@@ -1,11 +1,16 @@
 package com.aniwhere.domain.board.api;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller("boardViewController")
 @RequestMapping("/board")
 public class ViewController {
 
+    @GetMapping("/guide")
+    public String guide() {
+        return "anitalk/guide";
+    }
 
 }
