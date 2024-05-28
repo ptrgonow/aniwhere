6be6f1fc9abcb6c143da9db515b7c44517aa8137
patrick <lusielar@naver.com>
2024-05-28 09:44:15 +0900

    프로젝트 구조 설계 (#1) - patrick

--- src/main/java/com/aniwhere/domain/user/info/controller/UserRestController.java
+++ src/main/java/com/aniwhere/domain/user/info/controller/UserRestController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("userRestController")
-@RequestMapping("/info")
+@RequestMapping("/register")
 public class UserRestController {
 }
