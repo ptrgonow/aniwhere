21da873b86ac2e5acd4c29b40abd17a3ea2a9d2a
gem-studio <jun.johng@gmail.com>
2024-05-31 10:42:52 +0900

    프로젝트 구조 설계 (#1) - HOJUN

--- src/main/java/com/aniwhere/domain/shop/api/ViewController.java
+++ src/main/java/com/aniwhere/domain/shop/api/ViewController.java
@@ -20,15 +20,5 @@ public class ViewController {
     public String cart()  {return "animall/shop-cart";
     }
 
-    @GetMapping("/agreement")
-    public String getAgreement(Model model) {
-        Path filePath = Paths.get("src/main/resources/static/documents/mall_agreement.txt");
-        try {
-            String content = Files.readString(filePath);
-            model.addAttribute("agreementText", content);
-        } catch (IOException e) {
-            e.printStackTrace();
-            model.addAttribute("agreementText", "파일을 읽을 수 없습니다.");
-        }
-        return "agreement";
+
 }
