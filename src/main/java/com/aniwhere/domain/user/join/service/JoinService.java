e764c4d6becfaaa0d82513b436e2a69486b0720f
Patrick <lusielar@naver.com>
2024-07-03 18:20:42 +0900

    프로젝트 구조 설계 (#1) - patrick

--- src/main/java/com/aniwhere/domain/user/join/service/JoinService.java
+++ src/main/java/com/aniwhere/domain/user/join/service/JoinService.java
@@ -1,5 +1,6 @@
 package com.aniwhere.domain.user.join.service;
 
+import com.aniwhere.domain.user.join.dto.JoinDTO;
 import com.aniwhere.domain.user.mapper.UserMapper;
 import com.aniwhere.domain.user.join.domain.Join;
 import lombok.AllArgsConstructor;
@@ -17,6 +18,18 @@ public class JoinService {
 
     public void joinProcess(Join join) {
 
+        JoinDTO joinDTO = new JoinDTO();
+        joinDTO.setUserId(join.getUserId());
+        joinDTO.setUserPwd(passwordEncoder.encode(join.getUserPwd()));
+        joinDTO.setUserName(join.getUserName());
+        joinDTO.setEmail(join.getEmail());
+        joinDTO.setAddress(join.getAddress());
+        joinDTO.setDetailAddress(join.getDetailAddress());
+        joinDTO.setZipCode(join.getZipCode());
+        joinDTO.setPhone(join.getPhone());
+        joinDTO.setRole("ROLE_USER");
+
+        userMapper.register(joinDTO);
 
 
 
