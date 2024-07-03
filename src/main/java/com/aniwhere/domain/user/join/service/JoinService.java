f7b9fcea8f87a16de9c44a1187af847edd773d72
Patrick <lusielar@naver.com>
2024-07-03 18:19:13 +0900

    프로젝트 구조 설계 (#1) - patrick

--- src/main/java/com/aniwhere/domain/user/join/service/JoinService.java
+++ src/main/java/com/aniwhere/domain/user/join/service/JoinService.java
@@ -1,9 +1,8 @@
 package com.aniwhere.domain.user.join.service;
 
-import com.aniwhere.domain.user.mapper.UserMngMapper;
+import com.aniwhere.domain.user.mapper.UserMapper;
 import com.aniwhere.domain.user.join.domain.Join;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,12 +10,13 @@ import org.springframework.stereotype.Service;
 @AllArgsConstructor
 public class JoinService {
 
-    private final UserMngMapper userMngMapper;
+    private final UserMapper userMapper;
     private final BCryptPasswordEncoder passwordEncoder;
 
 
 
     public void joinProcess(Join join) {
+
 
 
 
