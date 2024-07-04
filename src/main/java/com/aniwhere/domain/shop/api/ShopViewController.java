f6a57ccad57ea8fae388cd2a87e0137d7d81af68
gem-studio <jun.johng@gmail.com>
2024-07-04 16:13:58 +0900

    프로젝트 구조 설계 (#1) - HOJUN

--- src/main/java/com/aniwhere/domain/shop/api/ShopViewController.java
+++ src/main/java/com/aniwhere/domain/shop/api/ShopViewController.java
@@ -1,12 +1,23 @@
 package com.aniwhere.domain.shop.api;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/shop")
 public class ShopViewController {
 
+    @GetMapping("/main")
+    public String shopMain()  {
+        return "animall/shop";
+    }
 
+    @GetMapping("/detail")
+    public String detail()  {
+        return "animall/single-product";
+    }
 
 }
