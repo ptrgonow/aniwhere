195de75af2ec6fc7b5e255f045ec79283a86fc03
DaHee <dahee215@naver.com>
2024-07-05 12:05:23 +0900

    회원가입 view (#1) - 다희

--- src/main/java/com/aniwhere/domain/main/MainController.java
+++ src/main/java/com/aniwhere/domain/main/MainController.java
@@ -33,14 +33,4 @@ public class MainController {
         return "index"; // index.html 템플릿으로 이동
     }
 
-    // TODO : 진석이한테 위치 변경하라고 할 것 (둘 다 board 로 이동)
-    @GetMapping("/faq")
-    public String faq() {
-        return "faq";
-    }
-
-    @GetMapping("/about")
-    public String about() {
-        return "about";
-    }
 }
