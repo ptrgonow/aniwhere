501d020de89d681fdc00033a0ae5d41a53cd320e
coconut9704 <yjs9704@naver.com>
2024-07-08 14:44:24 +0900

    프로젝트 구조 설계 (#1) - 진석

--- src/main/java/com/aniwhere/domain/board/notice/service/NoticeService.java
+++ src/main/java/com/aniwhere/domain/board/notice/service/NoticeService.java
@@ -1,8 +1,28 @@
 package com.aniwhere.domain.board.notice.service;
 
+import com.aniwhere.domain.board.notice.dto.NoticeDTO;
+import com.aniwhere.domain.board.notice.mapper.NoticeMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NoticeService {
 
+    private final NoticeMapper noticeMapper;
+
+    @Autowired
+    public NoticeService(NoticeMapper noticeMapper) {
+        this.noticeMapper = noticeMapper;
+    }
+
+    public List<NoticeDTO> getAllNotices() {
+        return noticeMapper.getAllNotices();
+    }
+
+    // 공지사항 등록
+    public void createNotice(NoticeDTO noticeDTO) {
+        noticeMapper.insertNotice(noticeDTO);
+    }
 }
