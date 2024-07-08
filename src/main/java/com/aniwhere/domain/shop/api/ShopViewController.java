6e6665a8159ee68db7d1938bf893aed28f42e3e3
gem-studio <jun.johng@gmail.com>
2024-07-08 18:19:17 +0900

    프로젝트 구조 설계 (#1) - HOJUN

--- src/main/java/com/aniwhere/domain/shop/api/ShopViewController.java
+++ src/main/java/com/aniwhere/domain/shop/api/ShopViewController.java
@@ -61,4 +61,9 @@ public class ShopViewController {
         return "animall/shop-checkout";
     }
 
-}
+    @GetMapping("/summary")
+        public String summary(){
+            return "animall/shop-summary";
+        }
+    }
+
