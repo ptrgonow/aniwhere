09c2dbe4cb5a149e8e6c73444afb86367cbc1f73
coconut9704 <yjs9704@naver.com>
2024-07-09 18:20:47 +0900

    프로젝트 구조 설계 (#1) - 진석

--- src/main/java/com/aniwhere/domain/board/notice/controller/NoticeRestController.java
+++ src/main/java/com/aniwhere/domain/board/notice/controller/NoticeRestController.java
@@ -24,11 +24,35 @@ public class NoticeRestController {
         return ResponseEntity.ok(notices);
     }
 
+    // 공지사항 상세 조회 및 조회수 증가
+    @GetMapping("/detail")
+    public ResponseEntity<NoticeDTO> getNoticeDetail(@RequestParam("id") Long noticeId) {
+        NoticeDTO notice = noticeService.getNoticeById(noticeId);
+        if (notice != null) {
+            return ResponseEntity.ok(notice);
+        } else {
+            return ResponseEntity.notFound().build();  // 공지사항이 없을 경우 404 응답
+        }
+    }
+
     // 공지사항 수정
     @PutMapping("/update")
     public ResponseEntity<Void> updateNotice(@RequestBody NoticeDTO noticeDTO) {
         noticeService.updateNotice(noticeDTO);
         return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
     }
+
+    // 삭제
+    @DeleteMapping("/board/delete-notice")
+    public ResponseEntity<String> deleteNotice(@RequestParam("id") Long noticeId) {
+        try {
+            noticeService.deleteNotice(noticeId.intValue());
+            return ResponseEntity.ok("삭제가 완료되었습니다.");
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("삭제 중 오류가 발생했습니다.");
+        }
+    }
+
+
 
 }
