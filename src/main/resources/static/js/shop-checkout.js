3980bc7f88710bdeeace3a252113565ea38a24e5
DaHee <dahee215@naver.com>
2024-07-09 17:45:48 +0900

    주문/결제 view 완성 (#2-2) - 다희

--- src/main/resources/static/js/shop-checkout.js
+++ src/main/resources/static/js/shop-checkout.js
@@ -1,7 +1,75 @@
 $(document).ready(function(){
     toss().then(r => console.log("done"));
+
+    // 스크롤 이벤트
+    const $sticky = $('.sticky');
+    const stickyTop = $sticky.offset().top;
+
+    $(window).scroll(function() {
+        const windowTop = $(window).scrollTop();
+
+        if (windowTop > stickyTop) {
+            $sticky.addClass('scrolled');
+        } else {
+            $sticky.removeClass('scrolled');
+        }
+    });
+
+    // 새로운 배송지 클릭 시 입력값 초기화
+    $(".new-del").on("click", function(event){
+        event.preventDefault();
+        event.stopPropagation(); // 아코디언 이벤트 막기
+        $("#shipping-form").find("input").val("");
+    });
+
 });
 
+// 주소 찾기
+function searchAdd() {
+    new daum.Postcode({
+        oncomplete: function(data) {
+            let addr = ''; // 주소
+            let extraAddr = ''; // 참고항목 변수
+
+            //사용자가 선택한 주소 타입에 따라 해당 주소 값을 가져온다.
+            if (data.userSelectedType === 'R') { // 사용자가 도로명 주소를 선택했을 경우
+                addr = data.roadAddress;
+            } else { // 사용자가 지번 주소를 선택했을 경우(J)
+                addr = data.jibunAddress;
+            }
+
+            // 사용자가 선택한 주소가 도로명 타입일때 참고항목을 조합한다.
+            if(data.userSelectedType === 'R'){
+                // 법정동명이 있을 경우 추가한다. (법정리는 제외)
+                // 법정동의 경우 마지막 문자가 "동/로/가"로 끝난다.
+                if(data.bname !== '' && /[동|로|가]$/g.test(data.bname)){
+                    extraAddr += data.bname;
+                }
+                // 건물명이 있고, 공동주택일 경우 추가한다.
+                if(data.buildingName !== '' && data.apartment === 'Y'){
+                    extraAddr += (extraAddr !== '' ? ', ' + data.buildingName : data.buildingName);
+                }
+                // 표시할 참고항목이 있을 경우, 괄호까지 추가한 최종 문자열을 만든다.
+                if(extraAddr !== ''){
+                    extraAddr = ' (' + extraAddr + ')';
+                }
+                // 조합된 참고항목을 해당 필드에 넣는다.
+                document.getElementById("detailAddress").value = extraAddr;
+
+            } else {
+                document.getElementById("detailAddress").value = '';
+            }
+
+            // 우편번호와 주소 정보를 해당 필드에 넣는다.
+            document.getElementById('address').value = data.zonecode;
+            document.getElementById("detailAddress").value = addr;
+            // 커서를 상세주소 필드로 이동한다.
+            document.getElementById("zipCode").focus();
+        }
+    }).open();
+}
+
+// 토스페이먼츠 결제위젯
 async function toss() {
     const button = document.getElementById("payment-button");
     const totalPriceElement = document.getElementById("total-price");
