72d573b6065a2f5df5ce6c37e72c59177956e4ca
hnsangjin <uk96dhk1@gmail.com>
2024-07-15 18:07:43 +0900

    OrderDTO 부활 (#1) - hsj

--- src/main/java/com/aniwhere/domain/shop/view/ShopViewController.java
+++ src/main/java/com/aniwhere/domain/shop/view/ShopViewController.java
@@ -105,6 +105,10 @@ public class ShopViewController {
             return "error";
         }
     }
+    @GetMapping("/success")
+    public String success(){
+        return "animall/shop-checkout-success";
+    }
 
     @GetMapping("/summary")
     public String summary(){
