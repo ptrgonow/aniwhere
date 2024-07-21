66a9950c71f402769a978030e72f3063dd31bca6
Patrick <lusielar@naver.com>
2024-07-21 21:34:35 +0900

    배포 테스트 (#9) - patrick

--- src/main/java/com/aniwhere/AniwhereApplication.java
+++ src/main/java/com/aniwhere/AniwhereApplication.java
@@ -1,9 +1,12 @@
 package com.aniwhere;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -16,5 +19,12 @@ public class AniwhereApplication extends SpringBootServletInitializer {
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
         return builder.sources(AniwhereApplication.class);
+    }
+
+    @Bean
+    public CommandLineRunner printActiveProfiles(Environment env) {
+        return args -> {
+            System.out.println("Active profiles: " + String.join(", ", env.getActiveProfiles()));
+        };
     }
 }
